7fbfbf61eeca06f20bd7d840c108809c03af7eb6
wstammSeidor <walterstamm92@gmail.com>
2024-09-21 01:36:27 -0300

    update

--- server.js
+++ server.js
@@ -6,15 +6,19 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
-
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -27,4 +31,8 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+//index route 
+app.get("/",function(req,res){
+  res.render("index",{title:"Hooooomeee"})
 })
