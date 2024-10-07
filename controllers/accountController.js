cfa064d7984e233cb37bfed63318489cf8e1b443
wstammSeidor <walterstamm92@gmail.com>
2024-10-07 19:57:14 -0300

    week4

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -18,14 +18,13 @@ async function buildLogin(req, res, next) {
 *  Deliver registration view
 * *************************************** */
 async function buildRegister(req, res, next) {
-    let nav = await utilities.getNav()
-    res.render("account/register", {
-      title: "Register",
-      nav,
-    })
-  }
-  
-  
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null,
+  })
+}
 
   
   /* ****************************************
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,10 +3,12 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register', regValidate.registationRules(), regValidate.checkRegData, utilities.handleErrors(accountController.registerAccount))
 
 
 module.exports = router
